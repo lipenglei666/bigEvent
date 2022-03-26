d4e3435dab2d838996d33517c80add7ef431ef5b
李鹏雷 <lpl2015@vip.qq.com>
2022-03-27 04:35:26 +0800

    主页显示用户基本信息完成

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,19 @@
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+  if (options.url.indexOf('my') != -1) {
+    options.headers = {
+      Authorization: localStorage['token']
+    };
+  }
+
+  options.complete = function (res) {
+    var data = res.responseJSON;
+    if (data.status == 1 && data.message == '身份认证失败！') {
+      localStorage.removeItem('token');
+      location.href = 'login.html';
+    }
+
+  }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+  // 导入layer
+  var layer = layui.layer;
+  getUserInfo();
+
+  // 退出
+  $('#exit').click(function(){
+    exit();
+  })
+  // 退出登录
+  function exit(){
+    layer.confirm('确定退出吗？',[3,'提示'],function(index){
+      layer.close(index);
+      localStorage.removeItem('token');
+      location.href='login.html'
+    })
+  }
+
+  // 获取用户信息
+  function getUserInfo() {
+    // 展示用户信息
+    $.ajax({
+      url: '/my/userinfo',
+      type: 'get',
+      // headers: {
+      //   Authorization: localStorage['token']
+      // },
+      success: function (res) {
+        if (res.status != 0) {
+          return layer.alert(res.message);
+        }
+        rederUserInfo(res);
+      }
+    });
+  }
+  // 渲染用户信息
+  function rederUserInfo(res) {
+    var uname = res.data.nickname || res.data.username;
+    $('.text-userName').html('欢迎&nbsp;&nbsp;' + uname);
+    var imgSrc = res.data.user_pic;
+    if (imgSrc) {
+      $('.headImg').show();
+      $('.avatar').hide();
+    } else {
+      $('.headImg').hide();
+      $('.avatar').show();
+      var text_avatar = uname[0].toUpperCase();
+      $('.text-avatar').html(text_avatar);
+    }
+  }
+
+})
